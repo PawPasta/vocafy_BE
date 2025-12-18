0d41f185c070b4bb53c56fc16c159a5d2e0bedde
PawPasta <giabaostrike2004@gmail.com>
2025-12-18 07:22:38 +0700

    first commit

---
+++ src/main/kotlin/com/exe/vocafy_BE/VocafyBeApplication.kt
@@ -0,0 +1,11 @@
+package com.exe.vocafy_BE
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VocafyBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<VocafyBeApplication>(*args)
+}
