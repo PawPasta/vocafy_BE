e8b4579cb37d59cda01ed34f9545ed641c6b064b
PawPasta <giabaostrike2004@gmail.com>
2026-01-21 07:45:31 +0700

    feat(payment) sepay integration

---
+++ src/main/kotlin/com/exe/vocafy_BE/util/SepayUtil.kt
@@ -0,0 +1,28 @@
+package com.exe.vocafy_BE.util
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+
+@Service
+class SePayUtil (
+
+    @param:Value("\${sepay.url}")
+    private val sePayQrUrl: String,
+
+    @param:Value("\${sepay.account}")
+    private val accountNumber: String,
+
+    @param:Value("\${sepay.brand}")
+    private val brandName: String,
+
+    @param:Value("\${sepay.template}")
+    private val template: String,
+
+){
+
+    fun generateSePayQrUrl(amount: Double, ref1: String): String {
+        return "$sePayQrUrl?acc=$accountNumber&bank=$brandName&amount=$amount&template=$template&des=$ref1"
+    }
+
+}
